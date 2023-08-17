ff7196cb127c5ca33584a34fd9ec1fe5d22c991d
sudhir <sudhirmishra5738@gamil.com>
2023-08-17 16:00:15 +0530

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+let inp=document.querySelector("input")
+let btn1=document.getElementById("btn1")
+let btn2=document.getElementById("btn2")
+let pera=document.getElementById("pera")
+let select=document.getElementById("select")
+
+let arr=[];
+btn1.onclick=()=>{
+   
+    arr.push(inp.value)
+    console.log(arr)
+    inp.value=""
+}
+btn2.onclick=()=>{
+
+   if(select.value=="Ascending"){
+    arr.sort((a,b)=>(a-b));
+    console.log(arr)
+    pera.innerHTML=arr;
+   };
+
+   if(select.value=="Descendimg"){
+    arr.sort((a,b)=>(b-a));
+    pera.innerHTML=arr;
+   }
+ 
+}
