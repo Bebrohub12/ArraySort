39fdfa619ddbaab7b2ef9d30938124650a82e2b9
Bebrohub12 <sudhirmishra5738@gmail.com>
2024-09-11 11:46:40 +0530

    https://github.com/rohitjainfsl/react-ecommerce.git

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-let inp=document.querySelector("input")
-let btn1=document.getElementById("btn1")
-let btn2=document.getElementById("btn2")
-let pera=document.getElementById("pera")
-let select=document.getElementById("select")
+let inp = document.querySelector("input")
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let pera = document.getElementById("pera")
+let select = document.getElementById("select")
 
-let arr=[];
-btn1.onclick=()=>{
-   
+let arr = [];
+btn1.onclick = () => {
+
     arr.push(inp.value)
     console.log(arr)
-    inp.value=""
-}
-btn2.onclick=()=>{
+    inp.value = ""
+};
 
-   if(select.value=="Ascending"){
-    arr.sort((a,b)=>(a-b));
-    console.log(arr)
-    pera.innerHTML=arr;
-   };
+btn2.onclick = () => {
 
-   if(select.value=="Descendimg"){
-    arr.sort((a,b)=>(b-a));
-    pera.innerHTML=arr;
-   }
- 
+    if (select.value == "Ascending") {
+        arr.sort((a, b) => (a - b));
+        console.log(arr)
+        pera.innerHTML = arr;
+    }
+    else if (select.value == "Descending") {
+        arr.sort((a, b) => (b - a));
+        pera.innerHTML = arr;
+    }
+    else {
+        alert("WRONG input SELECTED!!!!")
+    }
+
 }
